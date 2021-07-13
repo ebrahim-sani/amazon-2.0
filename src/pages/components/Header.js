fcc544ac1069ad3644389230d6995fa439a92126
ebrahim-sani <ibraheemsani035@gmail.com>
2021-07-14 00:30:44 +0100

    Setup/implement REDUX to Basket

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -5,9 +5,15 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../../slices/basketSlice";
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
+
   return (
     <header>
       {/* TOp nav */}
@@ -19,6 +25,7 @@ function Header() {
             height={40}
             objectFit="contain"
             className="cursor-pointer"
+            onClick={() => router.push("/")}
           />
         </div>
 
@@ -45,9 +52,12 @@ function Header() {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative flex items-center link">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative flex items-center link"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline mt-2 font-extrabold md:text-sm">
